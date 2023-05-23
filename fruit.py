61cc6ed6741761768a7578c6c2627ee223175701
TokyoGaijin <120317383+TokyoGaijin@users.noreply.github.com>
2023-05-23 17:19:51 +0900

    Basics fully workable - TODO: Scoreboard

--- fruit.py
+++ fruit.py
@@ -8,25 +8,17 @@ class Fruit:
         self.posX = 0
         self.posY = 0
         self.fruit_type = fruit_type
-        self.speed = 5
+        self.speed = 6
         self.fruit_sprite = pygame.image.load(os.path.join("fruit_sprites", f"{self.fruit_type}.png"))
         self.fruit_rect = pygame.Rect(self.posX, self.posY, self.fruit_sprite.get_width(), self.fruit_sprite.get_height())
         self.isVisible = True
 
-
-    def launch_fruit(self, startX, startY):
-        self.posX, self.posY = startX, startY
-        self.isVisible = True
-
+    def set_speed(self):
+        self.speed = random.randrange(6, 9)
 
     def update(self):
-        # if self.isVisible:
-        #     self.posY -= self.speed
-        #     self.speed -= .05
-        #     if self.posY <= random.randrange(0, 321) or self.speed <= 0:
-        #         self.speed += .05
-
         self.posY -= self.speed
+        self.speed -= .05
 
         self.fruit_rect.x = self.posX
         self.fruit_rect.y = self.posY
--- main.py
+++ main.py
@@ -7,14 +7,32 @@ game_win = screen_setting.Game_Screen()
 fruits = ["apple", "avocado", "banana", "cherry", "grapes", "orange", "strawberry"]
 fruit_list = []
 
-for i in range(0, len(fruits)):
-    fruit_list.append(fruit.Fruit(game_win.surface, fruit_type=fruits[i]))
+this_fruit = None
+
+def init():
+    global fruit_list
+    for i in range(0, len(fruits)):
+        fruit_list.append(fruit.Fruit(game_win.surface, fruit_type=fruits[i]))
+
+def get_fruit():
+    global this_fruit
+    this_fruit = fruit_list[random.randrange(0, len(fruit_list))]
+    this_fruit.posY = 500
+    this_fruit.posX = random.randrange(0, this_fruit.fruit_sprite.get_width())
+    this_fruit.isVisible = True
+    this_fruit.set_speed()
 
 
+def run():
+    init()
+    get_fruit()
+    while True:
+        game_win.update()
+        this_fruit.update()
+        this_fruit.draw()
+        if this_fruit.posY >= 500 or not this_fruit.isVisible:
+            get_fruit()
+            continue
 
-isGameOn = True
-
-while isGameOn:
-    game_win.update()
-    fruit_list[0].draw()
-    fruit_list[0].update()
+if __name__=='__main__':
+    run()

185479f95d83cc7ade8a19358dd8bb59f86d41d6
TokyoGaijin <120317383+TokyoGaijin@users.noreply.github.com>
2023-05-23 14:38:06 +0900

    Screen setting class DONE

---
+++ screen_setting.py
@@ -0,0 +1,28 @@
+import pygame
+import colorswatch as cs
+
+class Game_Screen:
+    def __init__(self, surface_name, title = "Untitled Game", sizeX = 640, sizeY = 480, bgColor = cs.cornflower_blue["pygame"]):
+        self.surface_name = surface_name
+        self.title = title
+        self.sizeX = sizeX
+        self.sizeY = sizeY
+        self.screen_size = (sizeX, sizeY)
+        self.bg = bgColor
+        self.surface = pygame.display.set_mode(self.screen_size)
+        self.CLOCK = pygame.time.Clock()
+        self.FPS = 60
+
+    def update(self):
+        self.CLOCK.tick(self.FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit(1)
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE]:
+            exit(1)
+
+        pygame.display.update()
+        self.surface.fill(self.bg)
